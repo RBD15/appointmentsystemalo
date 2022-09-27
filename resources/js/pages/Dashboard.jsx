532dfed6fffae2806d4a3578145fd5f92179ca53
RDStudio <nothinghere1984@gmail.com>
2022-09-27 14:46:37 -0400

    Laravel installed, laravel ui and login-register blades, a basic dashboard react componenet added and sqlite database driver setting in .env file

---
+++ resources/js/pages/Dashboard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Dashboard() {
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-8">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Dashboard;
+
+if (document.getElementById('dashboard')) {
+    ReactDOM.render(<Dashboard />, document.getElementById('dashboard'));
+}
