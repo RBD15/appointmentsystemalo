fe9731c35b12518a7e18e991cde04ada7cf25bd7
RDStudio <nothinghere1984@gmail.com>
2022-10-07 14:19:20 -0400

    [ADDED] new fields for validationUser response into PatientValidationController

--- app/Http/Controllers/AppointmentSystem/PatientValidationController.php
+++ app/Http/Controllers/AppointmentSystem/PatientValidationController.php
@@ -10,14 +10,12 @@ class PatientValidationController extends Controller
 {
     public function validationUser(Request $request){
 
-
         if($request->has('document') && $request->has('phone_number')){
             $result=Patient::where('document','=',$request->document)->get()->first();
             if($result->phone_number==$request->phone_number){
-                return response()->json(['message'=>'Patient Authorizated','name'=>$result->name,'plan'=>$result->plan_id],200);
+                return response()->json(['message'=>'Patient Authorizated','name'=>$result->name,'age'=>$result->age,'address'=>$result->address,'phone_number'=>$result->phone_number,'plan'=>$result->plan_id,'status'=>$result->status],200);
             }
         }
-
         return response()->json(['message'=>'Patient Unauthorizated'],401);
     }
 }
