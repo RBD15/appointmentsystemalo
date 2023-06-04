6b183f18019821e181c3d1354733dbbf3cb4cef7
RDStudio <nothinghere1984@gmail.com>
2023-06-04 15:07:59 -0400

    [EDIT] new user seed for administrator user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(1)->create([
+            'name' => 'Administrador',
+            'email' => 'r@correo.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => 'asg243dfdf',
+        ]);
         User::factory(6)->create();
         City::factory(6)->create();
         Speciality::factory(5)->create();
         Plan::factory(5)->create();
-        Doctor::factory(6)->create();       
+        Doctor::factory(6)->create();
         Patient::factory(6)->create();
         Appointment::factory(100)->create();
     }
